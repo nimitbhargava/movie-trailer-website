7d83aa2f591f24bb99a2bd8cd925e07c0383023e
Nimit Bhargava <nimitbhargava@gmail.com>
2017-07-05 22:01:11 +0530

    Added __doc__ and comments Replaced tabs with 4-space tab, not sure what causes this at Sublime Text 3 Need to pick up an IDE soon

--- media.py
+++ media.py
@@ -1,12 +1,25 @@
 import webbrowser
 
 
+# Class definition for Movie
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube):
+    """
+	This class when initialized require movie title, storyline, poster imag
+	e url, and youtube trailer link.
+	"""
+    def __init__(self, movie_title, movie_storyline, poster_image_url,
+                trailer_youtube):
+        """
+        Initialize movie title, storyline, poster image url, and youtube trail
+        er link of the movie.
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """opens the youtube trailer url in the default browser.opens the yout
+    	ube trailer url in the default browser."""
         webbrowser.open(self.trailer_youtube_url)
+
